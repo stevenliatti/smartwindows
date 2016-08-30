a8118a51287a68cbe769021f6c6f532e9e7e99d7
steven.liatti <steven.liatti@etu.hesge.ch>
2016-08-30 20:35:54 +0200

    J'ai continué à lire le tuto sur les socket php et tester

--- Test_php_sockets/server.php
+++ Test_php_sockets/server.php
@@ -18,3 +18,25 @@ if(!socket_bind($sock, "127.0.0.1", 5000)) {
 }
  
 echo "Socket bind OK \n";
+
+//listen
+socket_listen ($sock , 10);
+ 
+echo "Waiting for incoming connections... \n";
+ 
+//Accept incoming connection - This is a blocking call
+$client = socket_accept($sock);
+     
+//display information about the client who is connected
+if(socket_getpeername($client, $address, $port)) {
+    echo "Client $address : $port is now connected to us.";
+}
+ 
+//read data from the incoming socket
+$input = socket_read($client, 1024000);
+ 
+$response = "OK .. $input";
+ 
+// Display output  back to client
+socket_write($client, $response);
+socket_close($client);
