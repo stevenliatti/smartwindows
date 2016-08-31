45711b27750107b86ee40d24df221ee51be6f2d4
steven.liatti <steven.liatti@etu.hesge.ch>
2016-08-31 09:37:53 +0200

    Modif des tests sur les sockets en php J'ai abordé les "anciennes" fonctions de socket (celles en socket_) Je vais maintenant tester les "nouvelles" (celles avec stream_socket_ Le tuto provient toujours de binarytides.com

--- Workspace/Test_php_sockets/binarytides/socket_/client.php
+++ Workspace/Test_php_sockets/binarytides/socket_/client.php
@@ -10,44 +10,44 @@ $sock = socket_create(AF_INET, SOCK_STREAM, 0);
 
 if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
 {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
-     
-    die("Couldn't create socket: [$errorcode] $errormsg \n");
+	$errorcode = socket_last_error();
+	$errormsg = socket_strerror($errorcode);
+	
+	die("Couldn't create socket: [$errorcode] $errormsg \n");
 }
- 
+
 echo "Socket created \n";
 
 if(!socket_connect($sock, $ip_address, 80))
 {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
-     
-    die("Could not connect: [$errorcode] $errormsg \n");
+	$errorcode = socket_last_error();
+	$errormsg = socket_strerror($errorcode);
+	
+	die("Could not connect: [$errorcode] $errormsg \n");
 }
- 
+
 echo "Connection established \n";
 
 $message = "GET / HTTP/1.1\r\n\r\n";
- 
+
 //Send the message to the server
 if(!socket_send($sock, $message, strlen($message), 0)) {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
-     
-    die("Could not send data: [$errorcode] $errormsg \n");
+	$errorcode = socket_last_error();
+	$errormsg = socket_strerror($errorcode);
+	
+	die("Could not send data: [$errorcode] $errormsg \n");
 }
- 
+
 echo "Message send successfully \n";
- 
+
 //Now receive reply from server
 if(socket_recv($sock, $buf, 2045, MSG_WAITALL) === false) {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
-     
-    die("Could not receive data: [$errorcode] $errormsg \n");
+	$errorcode = socket_last_error();
+	$errormsg = socket_strerror($errorcode);
+	
+	die("Could not receive data: [$errorcode] $errormsg \n");
 }
- 
+
 //print the received message
 echo $buf . "\n------------- End of message -------------\n";
 
