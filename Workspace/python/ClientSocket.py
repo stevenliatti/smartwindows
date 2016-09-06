a065f514064a07170b0fffa93a5fcb4c638a2afe
raed.abdennad <raed.abdennadher@etu.hesge.ch>
2016-09-06 11:35:46 +0200

    new python files

--- Workspace/python/ClientSocket.py
+++ Workspace/python/ClientSocket.py
@@ -1,36 +1,7 @@
-import socket
 import MySQLdb
 import time
+import socket_functions as socket
 
-#socket connection
-def socket_open():
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((ip, port))
-        print "Ouverture de la socket"
-        return sock
-    except Exception as e:
-        print "Impossible d'ouvrir la socket: {}".format(e)
-
-#socket reception function
-def reception_socket(sock, ip, port):
-    try:
-        response = sock.recv(4)
-        while response != "DONE":
-            response = sock.recv(4)
-
-        date_now = "%d-%d-%d" % (time.localtime().tm_year,time.localtime().tm_mon,time.localtime().tm_mday)
-        time_now = "%d:%d:%d" % (time.localtime().tm_hour,time.localtime().tm_min,time.localtime().tm_sec)
-        print response + " on %s at %s" % (date_now, time_now)
-        for i in range(3):
-            response = sock.recv(1024)
-            print format(response)
-            data_array.append(response)
-        return data_array, date_now, time_now
-    except Exception as e:
-        print "Impossible de se connecter au serveur: {}".format(e)
-    finally:
-        sock.close()
 
 #database initialisation function
 def database_open(ip, login, password, database_name):
@@ -52,12 +23,10 @@ def insert_data(db, temp_int, temp_ext, luminosity, date, time):
         db.commit()
     except Exception as e:
         print format(e)
-    #except:
-        # Rollback in case there is any error
-        #db.rollback()
 
 
-data_array = []
+triple_data = []
+data_list = []
 date_now = ""
 time_now = ""
 
@@ -67,9 +36,16 @@ if __name__ == "__main__":
     ip, port = "192.168.32.241", 2000
 
     while 1:
-        sock = socket_open()
-        data_array, date_now, time_now = reception_socket(sock, ip, port)
+        
+        sock = socket.socket_open(ip, port)
+        
+        triple_data = socket.reception_socket(sock, ip, port)
+        
+        
+        socket.socket_close(sock)
+        
         db = database_open("localhost", "root", "", "smartwindows")
         insert_data(db, data_array[0], data_array[2], data_array[1], date_now, time_now)
-        data_array = []
         database_close(db)
+
+        data_array = []
---
+++ Workspace/python/socket_functions.py
@@ -0,0 +1,37 @@
+import socket
+
+#socket connection
+def socket_open(ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ip, port))
+        print "Ouverture du socket"
+        return sock
+    except Exception as e:
+        print "Impossible d'ouvrir le socket: {}".format(e)
+
+#socket reception message function
+def reception_socket(sock, ip, port):
+    try:
+        response = sock.recv(4)
+        while response != "DONE":
+            response = sock.recv(4)
+
+        for i in range(3):
+            response = sock.recv(1024)
+            print format(response)
+            data_array.append(response)
+        return data_array, date_now, time_now
+    except Exception as e:
+        print "Impossible de recevoir de messages: {}".format(e)
+
+#socket send message function
+def send_socket(sock, msg):
+    try:
+        sock.send(msg);
+    except Exception as e:
+        print "Impossible d'envoyer de messages: {}".format(e)
+
+#socket close
+def socket_close(sock):
+    sock.close()
