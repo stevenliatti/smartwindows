95ea3fe62d890c889b50462f9046da2465a3c431
steven.liatti <steven.liatti@etu.hesge.ch>
2016-09-13 22:41:51 +0200

    Ajout du masquage automatique si on coche auto en mode config

--- Workspace/web_app/control.php
+++ Workspace/web_app/control.php
@@ -36,6 +36,14 @@
 		function sliderValue(value) {
 			document.getElementById("range").innerHTML = value;
 		}
+
+		function show() {
+			document.getElementById("hide").removeAttribute("hidden");
+		}
+
+		function hide() {
+			document.getElementById("hide").setAttribute("hidden", true);
+		}
 	</script>
 </body>
 </html>
--- Workspace/web_app/form.php
+++ Workspace/web_app/form.php
@@ -1,19 +1,26 @@
 <form method="post" action="">
 	<p>Mode : <br>
-		<input type="radio" name="mode" value="0" id="auto" <?php echo $auto_checked; ?> ><label for="auto"> Automatique</label><br>
-		<input type="radio" name="mode" value="1" id="manual" <?php echo $manual_checked; ?> ><label for="manual"> Manuel</label>
+		<input type="radio" name="mode" value="0" id="auto" onclick="hide()" <?php echo $auto_checked; ?> ><label for="auto"> Automatique</label><br>
+		<input type="radio" name="mode" value="1" id="manual" onclick="show()" <?php echo $manual_checked; ?> ><label for="manual"> Manuel</label>
 	</p>
 
-	<p id="state_window">La fenêtre est : <br>
-		<input type="radio" name="window" value="1" id="open" <?php echo $open_checked; ?> ><label for="open"> Ouverte</label><br>
-		<input type="radio" name="window" value="0" id="close" <?php echo $close_checked; ?> ><label for="close"> Fermée</label>
-	</p>
+	<?php
+		$hide = "";
+		if ($auto_checked)
+			$hide = "hidden";
+	?>
 
-	<p> Le store est à ce niveau : <br>
-		<input id="slide" name="slide" type="range" min="0" max="10" value="<?php echo $blind_value; ?>" step="1" onchange="sliderValue(this.value)" />
-		<span id="range"><?php echo $blind_value; ?></span>
-	</p>
+	<div id="hide" <?php echo $hide; ?> >
+		<p id="state_window">La fenêtre est : <br>
+			<input type="radio" name="window" value="1" id="open" <?php echo $open_checked; ?> ><label for="open"> Ouverte</label><br>
+			<input type="radio" name="window" value="0" id="close" <?php echo $close_checked; ?> ><label for="close"> Fermée</label>
+		</p>
+
+		<p> Le store est à ce niveau : <br>
+			<input id="slide" name="slide" type="range" min="0" max="10" value="<?php echo $blind_value; ?>" step="1" onchange="sliderValue(this.value)" />
+			<span id="range"><?php echo $blind_value; ?></span>
+		</p>
+	</div>
 
 	<input type="submit" id="send_data" value="Valider">
-
 </form>
