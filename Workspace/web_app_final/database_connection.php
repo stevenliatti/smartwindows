e21923c10dfaa11c034a1188c37badd5694b665d
raed.abdennad <raed.abdennadher@etu.hesge.ch>
2016-09-15 14:17:48 +0200

    application web avant l'integration des graphes

--- Workspace/web_app_final/database_connection.php
+++ Workspace/web_app_final/database_connection.php
@@ -1,98 +1,125 @@
 <?php
-    //Ouverture de la connexion à la base de données
-    function database_open($adress, $user_name, $user_password, $database_name){
-        $conn = new mysqli($adress, $user_name, $user_password, $database_name);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
+	//Ouverture de la connexion à la base de données
+	function database_open($adress, $user_name, $user_password, $database_name){
+		$conn = new mysqli($adress, $user_name, $user_password, $database_name);
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		} 
 
-        return $conn;
-    }
-    
-    //Fermeture de la connexion à la base de données
-    function database_close($conn){
-        mysqli_close($conn);
-    }
+		return $conn;
+	}
+	
+	//Fermeture de la connexion à la base de données
+	function database_close($conn){
+		mysqli_close($conn);
+	}
 
-    //selection des données par jour
-    function day_data_select($conn, $day_begin, $time_begin = "00:00:00", $day_end = "", $time_end = "23:59:59")
-    {
-        if ($day_end == "")
-            $day_end = $day_begin;
-        echo "<h3>dans la fonction SELECT : <br> $day_begin<br> $time_begin<br> $day_end<br> $time_end";
+	//selection des données par jour
+	function day_data_select($conn, $day_begin, $time_begin = "00:00:00", $day_end = "", $time_end = "23:59:59")
+	{
+		if ($day_end == "")
+			$day_end = $day_begin;
+		echo "<h3>dans la fonction SELECT : <br> $day_begin<br> $time_begin<br> $day_end<br> $time_end";
 
-        $sql = "SELECT * FROM data WHERE date >= '".$day_begin."' AND date <= '".$day_end."' AND time >= '".$time_begin."' AND time <= '".$time_end."' ORDER BY date DESC, time DESC";
-        $result = $conn->query($sql);
-        
-        $array = [];
-        if ($result->num_rows > 0) {
-            // output data of each row
-            $i = 0;
-            while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-               $array[$i] = $row;
-               $i++;
-            }
-            return $array;
-        } else {
-            echo "<br>0 results";
-            return null;
-        }
-    }
+		$sql = "SELECT * FROM data WHERE date >= '".$day_begin."' AND date <= '".$day_end."' AND time >= '".$time_begin."' AND time <= '".$time_end."' ORDER BY date DESC, time DESC";
+		$result = $conn->query($sql);
+		
+		$array = [];
+		if ($result->num_rows > 0) {
+			// output data of each row
+			$i = 0;
+			while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+			   $array[$i] = $row;
+			   $i++;
+			}
+			return $array;
+		} else {
+			echo "<br>0 results";
+			return null;
+		}
+	}
 
-    //Sélection de la configuration utilisateur
-    function get_config($conn) {
-        $sql = "SELECT * FROM state ORDER BY id DESC LIMIT 0, 1";
-        $row = $conn->query($sql)->fetch_array(MYSQLI_ASSOC);
-        
-        $auto_checked = "";
-        $manual_checked = "";
-        $open_checked = "";
-        $close_checked = "";
+	//selection des données par mois : affichage de la moyenne, le min et le max de chaque jour
+	function month_data_select($conn, $month, $year)
+	{
+		$sql = "SELECT date, AVG(temp_int) AS 'avg_temp_int', MAX(temp_int) AS 'max_temp_int',
+				MIN(temp_int) AS 'min_temp_int', AVG(luminosity) AS 'avg_lum', MAX(luminosity) AS 'max_lum',
+				MIN(luminosity) AS 'min_lum', AVG(temp_ext) AS 'avg_temp_ext', MAX(temp_ext) AS 'max_temp_ext',
+				MIN(temp_ext) AS 'min_temp_ext', AVG(wind_speed) AS 'avg_wind_speed',
+				MAX(wind_speed) AS 'max_wind_speed', MIN(wind_speed) AS 'min_wind_speed'
+				FROM data
+				WHERE MONTH(date) = '".$month."' AND YEAR(date) = '".$year."'
+				GROUP BY date";
+		$result = $conn->query($sql);
 
-        //Condition pour le mode auto/manuel
-        if ($row['config_mode'] == "0")
-            $auto_checked = "checked";
-        else
-            $manual_checked = "checked";
+		$array = [];
+		if ($result->num_rows > 0) {
+			$i = 0;
+			while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+			   $array[$i] = $row;
+			   $i++;
+			}
+			return $array;
+		} else {
+			echo "<br>0 results";
+			return null;
+		}
+	}
 
-        //Condition pour l'ouverture/fermeture de la fenêtre
-        if ($row['window'] == "0")
-            $close_checked = "checked";
-        else
-            $open_checked = "checked";
+	//Sélection de la configuration utilisateur
+	function get_config($conn) {
+		$sql = "SELECT * FROM state ORDER BY id DESC LIMIT 0, 1";
+		$row = $conn->query($sql)->fetch_array(MYSQLI_ASSOC);
+		
+		$auto_checked = "";
+		$manual_checked = "";
+		$open_checked = "";
+		$close_checked = "";
 
-        $blind_value = $row['blind'];
+		//Condition pour le mode auto/manuel
+		if ($row['config_mode'] == "0")
+			$auto_checked = "checked";
+		else
+			$manual_checked = "checked";
 
-        $state = "État au : " . $row['date'] . " à " . $row['time'];
+		//Condition pour l'ouverture/fermeture de la fenêtre
+		if ($row['window'] == "0")
+			$close_checked = "checked";
+		else
+			$open_checked = "checked";
 
-        include("form.php");
-    }
+		$blind_value = $row['blind'];
 
-    // Enregistrement en base de données de l'état de la config
-    function save_config($conn) {
-        $mode = $_POST['mode'];
-        $auto_checked = $mode == "0" ? "checked" : "";
-        $manual_checked = $mode == "1" ? "checked" : "";
+		$state = "État au : " . $row['date'] . " à " . $row['time'];
 
-        $window = $_POST['window'];
-        $open_checked = $window == "1" ? "checked" : "";
-        $close_checked = $window == "0" ? "checked" : "";
+		include("form.php");
+	}
 
-        $blind_value = $_POST['slide'];
+	// Enregistrement en base de données de l'état de la config
+	function save_config($conn) {
+		$mode = $_POST['mode'];
+		$auto_checked = $mode == "0" ? "checked" : "";
+		$manual_checked = $mode == "1" ? "checked" : "";
 
-        date_default_timezone_set('Europe/Zurich');
-        $date = date("Y-m-d");
-        $time = date("H:i:s");
+		$window = $_POST['window'];
+		$open_checked = $window == "1" ? "checked" : "";
+		$close_checked = $window == "0" ? "checked" : "";
 
-        $sql = "INSERT INTO state (config_mode, window, blind, date, time, users_id) 
-                VALUES ('$mode', '$window', '$blind_value', '$date', '$time', '3')";
-        $conn->query($sql);
+		$blind_value = $_POST['slide'];
 
-        $state = "État au : " . $date . " à " . $time;
+		date_default_timezone_set('Europe/Zurich');
+		$date = date("Y-m-d");
+		$time = date("H:i:s");
 
-        include("form.php");
-    }
+		$sql = "INSERT INTO state (config_mode, window, blind, date, time, users_id) 
+				VALUES ('$mode', '$window', '$blind_value', '$date', '$time', '3')";
+		$conn->query($sql);
 
-    $db = database_open("127.0.0.1", "admin", "admin", "smartwindows");
+		$state = "État au : " . $date . " à " . $time;
+
+		include("form.php");
+	}
+
+	$db = database_open("127.0.0.1", "admin", "admin", "smartwindows");
 
 ?>
---
+++ Workspace/web_app_final/used_function.js
@@ -0,0 +1,164 @@
+// fonction de construction des charts
+// @param
+// 		ctx : le canvas qui va contenir le graphe
+//		title : le titre du graphe
+//		label_array : les labels des données (dans notre cas c'est la date et l'heure)
+// 		data_label1 : le libéllé des donneés du premier contexte
+// 		data_array1 : les données du premier contexte
+// 		color_string1 : la couleur du premier courbe
+// 		data_label2 : le libéllé des donneés du 2ème contexte
+// 		data_array2 : les données du 2ème contexte
+// 		color_string2 : la couleur du 2ème courbe
+function display_day(ctx, title, label_array, data_label1, data_array1, color_string1, data_label2 = null, data_array2 = null, color_string2 = null) {
+	var data_set = [
+		            {
+		            	label: data_label1,
+		                fill: false,
+		                lineTension: 0,
+		                backgroundColor: "rgba("+color_string1+",0.4)",
+		                borderColor: "rgba("+color_string1+",1)",
+		                borderCapStyle: 'butt',
+		                borderDash: [],
+		                borderWidth: 2,
+		                borderDashOffset: 0.0,
+		                borderJoinStyle: 'miter',
+		                pointBorderColor: "rgba("+color_string1+",1)",
+		                pointBackgroundColor: "#fff",
+		                pointBorderWidth: 5,
+		                pointHoverRadius: 5,
+		                pointHoverBackgroundColor: "rgba("+color_string1+",1)",
+		                pointHoverBorderColor: "black",
+		                pointHoverBorderWidth: 2,
+		                pointRadius: 1,
+		                pointHitRadius: 10,
+		                data: data_array1,
+		                spanGaps: false,
+		            }
+		        ];
+    if (data_array2 != null){
+    	data_set.push(
+    					{
+			    			label: data_label2,
+			                fill: false,
+			                lineTension: 0,
+			                backgroundColor: "rgba("+color_string2+",0.4)",
+			                borderColor: "rgba("+color_string2+",1)",
+			                borderCapStyle: 'butt',
+			                borderDash: [],
+			                borderWidth: 2,
+			                borderDashOffset: 0.0,
+			                borderJoinStyle: 'miter',
+			                pointBorderColor: "rgba("+color_string2+",1)",
+			                pointBackgroundColor: "#fff",
+			                pointBorderWidth: 5,
+			                pointHoverRadius: 5,
+			                pointHoverBackgroundColor: "rgba("+color_string2+",1)",
+			                pointHoverBorderColor: "black",
+			                pointHoverBorderWidth: 2,
+			                pointRadius: 1,
+			                pointHitRadius: 10,
+			                data: data_array2,
+			                spanGaps: false,
+						}
+					);
+    	}
+	var data = {
+		        labels: label_array,
+		        datasets: data_set
+		    };
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive:false,
+            title: {
+	            display: true,
+	            text: title,
+	            fullWidth : true,
+	            fontSize : 30
+	        },
+        }
+    });
+}
+
+
+// fonction de construction des charts
+// @param
+// 		ctx : le canvas qui va contenir le graphe
+function display_month(ctx, title, labels, array_moy, array_min, array_max, y_scale_min, y_scale_max) {
+	var data = {
+    labels: labels,
+    datasets: [
+	        {
+	            label: "Moyenne",
+	            backgroundColor: 'rgba(0, 102, 255, 0.2)',
+	            borderColor: 'rgba(0,102, 255,1)',
+	            borderWidth: 1,
+	            data: array_moy,
+	        },
+	        {
+	            label: "Min",
+	            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+	            borderColor: 'rgba(255,0,0,1)',
+	            borderWidth: 1,
+	            data: array_min,
+	        },
+	        {
+	            label: "Max",
+	            backgroundColor: 'rgba(0, 155, 0, 0.2)',
+	            borderColor: 'rgba(0,155,0,1)',
+	            borderWidth: 1,
+	            data: array_max,
+	        }
+    	]
+	};
+	var myBarChart = new Chart(ctx, {
+    	type: 'bar',
+    	data: data,
+    	options: {
+    		hover : {
+    			mode : "label"
+    		},
+            title: {
+	            display: true,
+	            text: title,
+	            fullWidth : true,
+	            fontSize : 30
+	        },
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    min : y_scale_min,
+	                    max : y_scale_max
+	                }
+	            }]
+	        }
+    	}
+	});
+}
+
+
+// fonction pour ajouter les champs de date et de temps si l'utilisateur
+// sélectionne l'option "tranche horaire"
+function change_content(value) {
+	selected_chart = document.getElementById("selected_chart");
+	date_element = document.getElementById("selected_date");
+	min_max_date_element = document.getElementById("selected_min_max_date");
+	month_element = document.getElementById("selected_month");
+	switch(value){
+		case "day_time":
+			date_element.style.display='none';
+			min_max_date_element.style.display='';
+			month_element.style.display = 'none';
+			break;
+		case "month":
+			date_element.style.display='none';
+			min_max_date_element.style.display='none';
+			month_element.style.display = '';
+			break;
+		default :
+			date_element.style.display='';
+			min_max_date_element.style.display='none';
+			month_element.style.display = 'none';
+	}
+}
